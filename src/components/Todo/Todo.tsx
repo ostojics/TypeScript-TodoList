15576e1fef829deb0ac68cd0b5f8613eecfa5a27
ostojics <ostojic.dev@gmail.com>
2021-07-06 23:21:28 +0200

    Minor code improvement

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -4,14 +4,14 @@ import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import './Todo.css';
 
 const Todo: React.FC<{ todoText: string, clickHandler: () => void }> = (props) => {
-    const { todoText } = props;
+    const { todoText, clickHandler } = props;
 
     return (
         <div className='todo'>
             <div className='todo-text'>
                 <p>{ todoText }</p>
             </div>
-            <div className='todo-icon' onClick={ props.clickHandler }>
+            <div className='todo-icon' onClick={ clickHandler }>
                 <FontAwesomeIcon icon={ faTrashAlt }/>
             </div>
         </div>
